1e617bdd692fadf70d4e147c99d52c28a6baec5d
superj-v1 <137361781+superj-v1@users.noreply.github.com>
2025-04-14 13:11:46 -0300

    Update do player para ver se isso funfa
    talvez a normalização seja o problema

--- player1.gd
+++ player1.gd
@@ -1,3 +1,10 @@
 extends CharacterBody2D
 
-var direcao = Vector2();
+@export var velocidade = 500
+
+func detec_imput():
+    var direcao = input.get_vector("left". "right", "up", "down")
+    velocity = direcao.normalize() * velocidade
+func _process_physics(delta):
+    detec_imput()
+    move_and_slide()
